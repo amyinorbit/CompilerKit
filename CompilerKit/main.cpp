9cc55569eee4f4707bcf1767bc57fdc42f8d8bd7
Amy Parent <amy@amyparent.com>
2021-11-09 12:08:45 +0000

    Initial Commit

---
+++ CompilerKit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CompilerKit
+//
+//  Created by Amy Parent on 09/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
